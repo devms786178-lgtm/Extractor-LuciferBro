2e314724385ee7fce88f08a08c2ea0f1a7d6e7da
devms786178-lgtm <devms786178@gmail.com>
2026-06-18 19:38:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Extractor-LuciferBro
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/hackercoder24/cobraupdate
+)
+
+
+                     
---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+from os import getenv
+
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "25318125"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","b29fb6a928e8b8a3308f8c2d3ba9cfb0")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@LUCIFEREXTRACTORBOT")
+BOT_TEXT = "TXT EXTRACTOR"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "7764674199"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002610898117"))
+
+# //FORCE_CHANNEL_ID
+#CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002799217873")) 
+# -----------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002844381920"))
+# -----------------------------------------------
+join = '<a href="https://t.me/urs_lucifer">✳️ Bᴀᴄᴋᴜᴘ</a>'
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "LUCIFEREXTRACTORBOT" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2fbd9fda0f646b1422f05-218a2421d48d601d10.jpg")
+
+
+
+
+# # Bot configuration
+# API_ID = int(os.environ.get("API_ID", "22746239"))
+# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
+
